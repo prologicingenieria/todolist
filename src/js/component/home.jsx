356e6cf55c52a48b32560344b0f829354def4d4a
Pedro López Murúa <pedro.lopez@prologic.cl>
2023-03-25 12:59:27 +0000

    to do list v2

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,25 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+//import react into the bundle
+import React, { useState } from "react";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  return (
+    <div className="container box text-center col-lg-12">
+      <div className="row">
+        <div className="col p-5">
+          <h1 className="p-5"> TO DO LIST</h1>
+          <input type="text" />
+          <button className="btn btn-success">agregar</button>
+        </div>
+	</div>
+     <div className="col p-5"></div>
+      
+
+	<div className="receptor">
+  </div>
+
+    </div>
+  );
 };
 
 export default Home;
