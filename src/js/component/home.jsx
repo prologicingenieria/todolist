b42a67f23370a1fc38ce31595dd1a4f991c94d5a
Pedro López Murúa <pedro.lopez@prologic.cl>
2023-03-25 13:37:56 +0000

    to do list

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,62 @@
-//import react into the bundle
-import React, { useState } from "react";
+import React, { useState } from 'react';
 
-//create your first component
-const Home = () => {
+
+function Home() {
+  // Declaración del estado del componente para almacenar las tareas
+  const [tasks, setTasks] = useState([]);
+  // Declaración del estado del componente para almacenar la entrada de usuario
+  const [inputValue, setInputValue] = useState('');
+
+  // Función para manejar el envío del formulario
+  function handleSubmit(event) {
+    event.preventDefault(); // Prevención de comportamiento predeterminado del formulario
+    if (inputValue !== '') { // Verificación de que la entrada no está vacía
+      // Creación de un nuevo objeto de tarea
+      const newTask = { id: Date.now(), title: inputValue };
+      // Actualización del estado de las tareas mediante la adición del objeto creado
+      setTasks([...tasks, newTask]);
+      // Reinicio del estado de la entrada
+      setInputValue('');
+    }
+  }
+
+  // Función para manejar el cambio en la entrada de usuario
+  function handleChange(event) {
+    setInputValue(event.target.value); // Actualización del estado de la entrada
+  }
+
+  // Función para eliminar una tarea seleccionada
+  function handleDelete(id) {
+    // Actualización del estado de las tareas mediante la eliminación de la tarea seleccionada
+    setTasks(tasks.filter((task) => task.id !== id));
+  }
+
   return (
-    <div className="container box text-center col-lg-12">
-      <div className="row">
-        <div className="col p-5">
-          <h1 className="p-5"> TO DO LIST</h1>
-          <input type="text" />
-          <button className="btn btn-success">agregar</button>
-        </div>
-	</div>
-     <div className="col p-5"></div>
-      
-
-	<div className="receptor">
-  </div>
-
+    <div className="container">
+      <h1>To Do List</h1>
+      <form onSubmit={handleSubmit} className="form">
+        <input
+          type="text"
+          placeholder="Ingrese una nueva tarea
+          ..."
+          value={inputValue}
+          onChange={handleChange}
+        />
+        <button type="submit">Agregar</button>
+      </form>
+      <ul>
+        {/* Renderizado de cada tarea en la lista */}
+        {tasks.map((task) => (
+          <li key={task.id}>
+            <p>{task.title}</p>
+            <button onClick={() => handleDelete(task.id)}>Eliminar</button>
+          </li>
+        ))}
+      </ul>
+      {/* Contador de tareas */}
+      <p>Total de Tareas: {tasks.length}</p>
     </div>
   );
-};
+}
 
 export default Home;
